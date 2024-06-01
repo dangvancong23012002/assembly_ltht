18eee9e437d96898b3bdd166445006e13263ac4e
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-06-01 22:48:47 +0700

    edit bai tap a mu n

---
+++ lt1.cpp
@@ -0,0 +1,29 @@
+#include <iostream.h>
+#include <conio.h>
+int a,n;
+extern int LT();
+void main(void)
+{
+	char tl;
+	L1:
+		clrscr();
+		cout << "\nLIEN KET VOI NGON NGU BAC CAO(C++)";
+		cout << "\n----------------------------------";
+		cout << "\n\nBai tap: Viet chuong trinh tinh a mu n (a: nguyen duong, n: nguyen duong) ";
+		cout << "\nvoi su phan cong sau: ";
+		cout << "\n* Module C++:";
+		cout << "\n - Nhan gia tri a va n,";
+		cout << "\n - Goi ham tinh a mu n do Assembly viet va hien ket qua";
+		cout << "\n* Module Assembly: Viet ham tinh a mu n.";
+		cout << "\n\n-----------------------------------";
+		cout << "\n----------CHUONG TRINH-------------";
+		cout << "\nHay vao a : "; cin >> a;
+		cout << "Hay vao n : "; cin >> n;
+		cout << a << " luy thua " << n << " la : " << LT();
+		cout << "\n-----------------------------------";
+	cout << "\nCo tiep tuc(c/k)?";
+	tl = getch();
+	if (tl == 'c') {
+		goto L1;
+	}
+}
